4388cd3cd36372fa05d3267be941517ead8d6c09
Andy Thawng <thawngandy@gmail.com>
2025-07-09 17:36:40 +0630

    Final Touch Update

---
+++ js/global.js
@@ -0,0 +1,5 @@
+
+  function changeLang(lang) {
+    alert("Language switched to: " + (lang === 'en' ? 'English' : 'မြန်မာ'));
+    // You can add actual text-switching logic here if you want later
+  }
